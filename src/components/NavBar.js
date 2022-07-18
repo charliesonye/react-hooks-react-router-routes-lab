54a23214e8e92961388be39f68235d7ad059b464
charliesonye <90803892+charliesonye@users.noreply.github.com>
2022-07-18 15:02:12 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,44 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const linkStyles = {
-  display: "inline-block",
-  width: "50px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "black",
-  textDecoration: "none",
-  color: "white",
-  
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
 }
 
-const NavBarBackdropStyles = {
-  display:"inline-block",
-  background:" grey",
-  wordSpacing:"50px",
-  width:"100%"
- 
-
-}
-
-function NavBar() {
-  return (
-    <div style={NavBarBackdropStyles}>
-      <ul>
-      <NavLink to='/' style={linkStyles}>
-        <li>Home</li>
-      </NavLink>
-      <NavLink to='/movies' style={linkStyles}>
-        <li>Movies</li>
-      </NavLink>
-      <NavLink to='/directors' style={linkStyles}>
-        <li>Directors</li>
-      </NavLink>
-      <NavLink to='/actors' style={linkStyles}>
-        <li>Actors</li>
-      </NavLink>
-      </ul>
+function Navigation(){
+ return(
+    <div>
+        <NavLink 
+            to="/" 
+            exact 
+            style={linkStyles} 
+            activeStyle={{
+            background: "darkblue"
+            }} 
+        >
+            Home
+        </NavLink>
+        <NavLink 
+            to="/movies" 
+            exact 
+            style={linkStyles} 
+            activeStyle={{
+                background: "darkblue"
+                }} 
+        >
+            Movies
+        </NavLink>
     </div>
-  )
+    
+    
+ )  
+
 }
 
-export default NavBar;
+export default Navigation
