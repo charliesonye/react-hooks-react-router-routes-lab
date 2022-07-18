2da16f78801a2e61fcb3f4b61cc67905f6cf9ba9
charliesonye <90803892+charliesonye@users.noreply.github.com>
2022-07-18 15:03:42 -0700

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,45 +1,14 @@
-import React from "react";
-import { movies } from "../data";
+import React, { useEffect, useState } from 'react'
 
-
-function Movies() {
-
-  const movie= movies.map((movieObj)=> {
+function Movies(){
    
-    
 
-    return(
-      <div key={movieObj.title}  >
-        <h2>Name: {movieObj.title} </h2>
-        <>Time:{ movieObj.time}</>
-        <br />
-        <br />
-        Genres:
-        <br />
-        <div>
-        <ul>
-          {movieObj.genres.map((genre)=>{
-            return <li key= {genre} >{genre}</li>
-            })
-         
-          }
-        </ul>
-        <hr />
-        </div>
-     
-      </div>
-
-      
-    )
-  })
  
-  return (
-   <div>
-    <h1>Movies Page</h1>
-    <br />
-    {movie}
-   </div>
-  )
+    return(
+        <div>
+            <h1> My Movies</h1>
+        </div>
+    )
 }
 
 export default Movies;
